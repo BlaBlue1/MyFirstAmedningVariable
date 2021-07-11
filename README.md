21d30f2cfe021aa66e6568a2dddcc374df8e0a29
RuthWudu <ruthwudu576@gmail.com>
2021-07-11 00:56:23 -0400

    descriptive message

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-#My First java Program
+#This is my first Pseudocode
+##How much water runs off a roof in a rainstorm?
+Begin
+
+Get dimensions of the footprint of the roof
+Get the number of inches of rainfall
+
+Prompt user to get dimensions of the footprint of the roof
+Get dimensions of the footprint of the roof
+
+Prompt user to get the number of inches of rainfall
+     Get the number of inches of rainfall
+
+ 
+
+    Total runoff per gallon = (dimensions of the footprint of the roof * the number of inches of rainfall)/231
+
+
+
+End
+
+Test data:
+
+
+
+
+
--- src/MyFirstVariable.java
+++ src/MyFirstVariable.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class MyFirstVariable {
     public static void main(String[] args)
     {
+        Scanner keyboard = new Scanner(System.in);
 
+        int num1, num2;
         int annualSalary = 62000;
         float increment = 0.1F;
         float firstYearSalary = annualSalary + (annualSalary* increment);
@@ -11,8 +13,17 @@ public class MyFirstVariable {
         float thirdYearSalary = secondYearSalary + (secondYearSalary * increment);
         float fourthYearSalary = thirdYearSalary + (thirdYearSalary * increment);
         float fifthYearSalary = fourthYearSalary + (fourthYearSalary * increment);
-        System.out.print(fifthYearSalary);
-        
+        System.out.print("Fifth year salary is: " + fifthYearSalary );
+        keyboard.nextLine();
+
+        System.out.print("What is your current Salary? ");
+        num1 = keyboard.nextInt();
+        keyboard.nextLine();
+
+        System.out.print("How many years did you work? ");
+        num2 = keyboard.nextInt();
+        keyboard.nextLine();
+
 
 
     }
